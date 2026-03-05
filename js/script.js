dca3a51d362b1c7c61f38976b644ba8403a4dd25
Bohdan Karaziia <bodyakaraziya1@gmail.com>
2026-03-05 11:51:01 +0200

    bug Fix

--- js/script.js
+++ js/script.js
@@ -263,21 +263,31 @@ if (tableBodyStudents) {
                 const tr = document.createElement("tr");
                 tr.dataset.id = nextStudentId++;
                 tr.innerHTML = `
-                    <td><input type="checkbox" class="select-student"></td>
+                    <td>
+                        <input type="checkbox" class="select-student" aria-label="Select student">
+                    </td>
                     <td>${groupVal}</td>
                     <td>${fullName}</td>
                     <td>${genderVal}</td>
                     <td>${convertDateToTableFormat(birthdayVal)}</td>
-                    <td><span class="status status--offline"></span></td>
                     <td>
-                        <button class="btn-edit">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="edit-icon" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" /></svg>
+                        <span class="status status--online status--offline" aria-label="Status"></span>
+                    </td>
+                    <td>
+                        <button class="btn-edit" aria-label="Edit student">
+                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="edit-icon" viewBox="0 0 16 16">
+                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                            </svg>
                         </button>
-                        <button class="btn-delete">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="delete-icon" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" /></svg>
+                        <button class="btn-delete" aria-label="Delete student">
+                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="delete-icon" viewBox="0 0 16 16">
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                            </svg>
                         </button>
                     </td>
-                `;
+                    `;
                 tableBodyStudents.appendChild(tr);
             } else if (mode === "edit" && idEditingRow) {
                 const row = document.querySelector(`tr[data-id="${idEditingRow}"]`);
@@ -303,7 +313,7 @@ if (tableBodyStudents) {
             idDeletingRow = null; 
             
             if (spanDeleteUserName) {
-                spanDeleteUserName.textContent = "all selected users";
+                spanDeleteUserName.textContent = "all selected students";
             }
             
             modalDelete.style.display = "flex";
